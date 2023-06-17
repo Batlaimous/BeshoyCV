1c8e4590cc5fb2161e28deecea95b3961974c64b
beshoy_younan <beshoy.younan@outlook.com>
2023-06-17 14:16:44 +0300

    add repositries work with controllers and views

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Entities;
+using Core.Interfaces;
+using InfraStructure.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork<Owner> _owner;
+        private readonly IUnitOfWork<PortfolioItem> _portfolio;
+
+        public HomeController(IUnitOfWork<Owner> owner, IUnitOfWork<PortfolioItem> portfolio)
+        {
+            _owner = owner;
+            _portfolio = portfolio;
+        }
         public IActionResult Index()
         {
             return View();
---
+++ WebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+using System.Collections.Generic;
+
+namespace WebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Owner Owner { get; set; }
+        public List<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
